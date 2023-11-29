2cbd75e8980d7d12257528df6e4ac4e0a3b2679f
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-29 18:20:20 -0500

    Added new cursed version of selection manager

--- My project/Assets/Scripts/Managers/SelectionManager.cs
+++ My project/Assets/Scripts/Managers/SelectionManager.cs
@@ -7,6 +7,10 @@ public class SelectionManager : MonoBehaviour
     [SerializeField] private PotionController potion1;
     [SerializeField] private PotionController potion2;
     [SerializeField] private PotionController potion3;
+    [SerializeField] private PotionControllerLV1 LV1potion1;
+    [SerializeField] private PotionControllerLV1 LV1potion2;
+    [SerializeField] private PotionControllerLV1 LV1potion3;
+    
     public List<GameObject> remainingPotions;
     public int selectedPotion = 1;
     private bool axisInUse = false;
@@ -64,7 +68,9 @@ public class SelectionManager : MonoBehaviour
 
     private void HighlightSelectedPotion()
     {
-        for (int i = 0; i < remainingPotions.Count; i++)
+        if (potion1 != null && potion2 != null && potion3 != null)
+        {
+            for (int i = 0; i < remainingPotions.Count; i++)
         {
             if (remainingPotions[i] != null)
             {
@@ -74,7 +80,23 @@ public class SelectionManager : MonoBehaviour
                     remainingPotions[i].GetComponent<PotionController>().potionSelected = true;
                 }
             }
-
         }
+        }
+
+        if (LV1potion1 != null && LV1potion2 != null && LV1potion3 != null)
+        {
+            for (int i = 0; i < remainingPotions.Count; i++)
+        {
+            if (remainingPotions[i] != null)
+            {
+                remainingPotions[i].GetComponent<PotionControllerLV1>().potionSelected = false;
+                if (i == selectedPotion - 1)
+                {
+                    remainingPotions[i].GetComponent<PotionControllerLV1>().potionSelected = true;
+                }
+            }
+        }
+        }
+        
     }
 }
